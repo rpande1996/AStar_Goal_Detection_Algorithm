cbdfb59a6e937bac641ac67cb18924ea0707774e
Abhilash Mane <37464530+abhilash1998@users.noreply.github.com>
2021-04-06 13:41:30 -0400

    Add files via upload
    Initial import and map creation

---
+++ AStar.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 19:15:58 2021
+
+@author: abhi
+"""
+
+from Exploration import exploration_r
+import numpy as np
+import cv2
+
+image=255*np.ones((300,400,3))
+#print(image)
+
+parent_orignal_data=[]
