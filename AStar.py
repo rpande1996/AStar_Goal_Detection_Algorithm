d1c0e287a85979a3b60d0ca0ecd063f5f601cd80
Abhilash Mane <37464530+abhilash1998@users.noreply.github.com>
2021-04-06 13:44:14 -0400

    Add files via upload
    Added lines to get user input and define the states. Calling the exploration class for the input values

--- AStar.py
+++ AStar.py
@@ -14,3 +14,27 @@ image=255*np.ones((300,400,3))
 #print(image)
 
 parent_orignal_data=[]
+
+#Inputting coordinates
+
+print(" Start coordinates ")
+s_y=input(" x_coordinate : ")
+s_x=input(" y_coordinate : ")
+theta_s=int((input(" theta_start : ")))
+print(" Goal coordinates ")
+g_y=input(" x_coordinate : ")
+g_x=input(" y_coordinate : ")
+theta_g=int(input(" theta_goal : "))
+print(" Other parameters ")
+step=int(input(" step size : "))
+
+#print(s_x,s_y,g_x,g_y)0
+start=[int(s_x),int(s_y)]
+goal=[int(g_x),int(g_y)]
+i=0
+image=image.astype(np.uint8)
+
+# object of class
+exploration=exploration_r(start,goal,step,theta_s,theta_g)
+
+exploration.obstacles_form(image)
